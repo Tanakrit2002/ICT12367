dd5c485f4d4afb6a83f418d60ef0e547ba1256d6
Tanakrit2002 <tanakrit.udomsiri2002@gmail.com>
2026-04-19 15:02:12 +0700

    Lab12

---
+++ Lab12/myproject/myapp/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render,redirect, get_object_or_404
+from django.http import HttpResponse
+from myapp.models import Person
+
+# Create your views here.
+def index(request):
+    all_Person = Person.objects.all()
+    return render(request, 'index.html',{"all_person":all_Person})
+# Create your views here.
+def about(request):
+    return render(request, 'about.html')
+
+# def form(request):
+    # return render(request, 'form.html')
+
+def form(request) :
+    if request.method == "POST" :
+        # รับข้อมูลจากฟอร์ม
+        name = request.POST.get("name")
+        age = request.POST.get("age")
+        # บันทึกลงฐานข้อมูล
+        person = Person.objects.create(
+            name=name,
+            age=age
+        )
+        # เปลี่ยนเส้นทางไปหน้าเเรก
+        return redirect("/")
+    else:
+
+        # เเสดงฟอร์ม
+        return render(request, 'form.html')
+
+def edit(request, person_id):
+    person = get_object_or_404(Person, id=person_id)
+    
+    if request.method == "POST":
+        person.name = request.POST.get("name")
+        person.age = request.POST.get("age")
+        person.save() 
+        return redirect("/")       
+    return render(request, 'edit.html', {'person': person})
